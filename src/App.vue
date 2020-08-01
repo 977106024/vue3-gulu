c717e82e95d651af695551cac5777b78ecc4a695
bohong <977106024@qq.com>
2020-08-01 18:02:33 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>hello</h1>
+  <ahong/>
+</template>
+
+<script>
+import ahong from './components/ahong.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ahong
+  }
+}
+</script>
---
+++ src/components/ahong.vue
@@ -0,0 +1,9 @@
+<template>
+<p>是我！</p>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
